edc4433d0efecdc78f7b3441f2d64db165b4cf56
Mithun Bose <mithbose@amazon.com>
2018-10-08 08:59:21 +0530

    Atsro unfinished commit

---
+++ src/java/com/astro/test/supportscheduleclient/client/Client.java
@@ -0,0 +1,109 @@
+package com.astro.test.supportscheduleclient.client;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceRef;
+
+/**
+ *
+ * @author mithbose
+ */
+@WebServlet(urlPatterns = {"/Client"})
+public class Client extends HttpServlet {
+    
+   /* TODO: make proper UI to display things.
+    
+    These rules are liable to change in the future, so make sure your design is flexible 
+enough to be able to add new rules.
+    
+    
+    */
+
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/SupportSchedulerService/SupportSchedulerService.wsdl")
+    private SupportScheduleService service;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String name = request.getParameter("nm");
+            String supportSchedule = getSupportSchedule(name);
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Client</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Obtained from server " + supportSchedule + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private String getSupportSchedule(java.lang.String name) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        SupportScheduleService port = service.getSupportSchedulerServicePort();
+        return port.getSupportSchedule(name);
+    }
+
+}
